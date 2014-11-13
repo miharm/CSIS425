9dec80a8110ffae752d3a7e2dcf91b1bdbde15e7
miharm <mharmon010@gmail.com>
2014-11-13 17:40:19 -0600

    Initial creation

---
+++ CSIS425/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CSIS425.MvcApplication" Language="C#" %>
---
+++ CSIS425/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSIS425.Startup))]
+namespace CSIS425
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
