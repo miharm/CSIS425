3120453ef27ba994761b52785ffdc869332fb88e
miharm <mharmon010@gmail.com>
2014-11-13 19:20:42 -0600

    DB stuff

---
+++ CSIS425/Controllers/Controller_Create_User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSIS425.Controllers
+{
+    public class Controller_Create_User : Controller
+    {
+    }
+}
