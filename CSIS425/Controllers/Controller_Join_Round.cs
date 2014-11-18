57a9e95a8928a7d7661f831ef9772cf5c564d553
miharm <mharmon010@gmail.com>
2014-11-17 19:14:41 -0600

    Front Controller Code

--- CSIS425/Controllers/Controller_Join_Round.cs
+++ CSIS425/Controllers/Controller_Join_Round.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Collections.Specialized;
 
 namespace CSIS425.Controllers
 {
-    public class Controller_Join_Round : Controller
+    public class Controller_Join_Round : Base_Controller
     {
+        public void run(NameValueCollection parameters)
+        {
+
+        }
     }
 }
---
+++ CSIS425/Handler/Handler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections.Specialized;
+using System.Web.Mvc;
+using CSIS425.Controllers;
+
+namespace CSIS425.Handler
+{
+    public class Handler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            NameValueCollection parameters = context.Request.Params;
+
+            Base_Controller controller;
+
+            switch ( parameters["action"] )
+            {
+                case "manage_create_course":
+                    controller = new Controller_Create_Course();
+                    break;
+                case "manage_create_user":
+                    controller = new Controller_Create_User();
+                    break;
+                case "manage_join_round":
+                    controller = new Controller_Join_Round();
+                    break;
+                case "manage_score_card":
+                    controller = new Controller_Score_Card();
+                    break;
+                default:
+                    controller = new Controller_Invalid_Action();
+                    break;
+            }
+
+            controller.run(parameters);
+
+        }
+    }
+}
