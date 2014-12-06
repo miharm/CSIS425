369bdc247dbcdfe82333d26964ea2b6c0f243be6
Jessica McCollum <mccollum.jm@gmail.com>
2014-12-06 14:27:11 -0600

    Update

---
+++ CSIS425.Tests/ModelTesting/CourseTesting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CSIS425;
+using CSIS425.Models.Model_Courses;
+
+namespace CSIS425.Tests.ModelTesting
+{
+    [TestFixture]
+    class CourseTesting
+    {
+
+        [Test]
+        public void HolesTest(){
+            Model_Courses c = new Model_Courses();
+            c.holes() = 9;
+            Assert.AreEqual(9, c.holes());
+            
+
+        }
+    }
+}
